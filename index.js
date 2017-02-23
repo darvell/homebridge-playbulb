7286748c2df8c1d6ecae4232053233580d49217e
Martin Rosinski <martin@zone-mr.net>
2017-02-23 15:51:46 +0000

    Added auto power off timer feature

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var HomeSysPlatform = require('./lib/HomeSysPlatform.js');
 var PlaybulbAccessory = require('./lib/PlaybulbAccessory.js');
 
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
+	HomeSysPlatform.setHomebridge(homebridge);
     PlaybulbAccessory.setHomebridge(homebridge);
 
     homebridge.registerAccessory("playbulb", "Playbulb", PlaybulbAccessory);
