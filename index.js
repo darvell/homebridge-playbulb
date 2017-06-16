ca5c9047018778b3512351fd1b3337a10e1d3dc3
Darvell Long <contact@darvell.me>
2017-06-16 16:46:49 -0700

    remove timer

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var HomeSysPlatform = require('./lib/HomeSysPlatform.js');
-var PlaybulbAccessory = require('./lib/PlaybulbAccessory.js');
+var HomeSysPlatform = require("./lib/HomeSysPlatform.js");
+var PlaybulbAccessory = require("./lib/PlaybulbAccessory.js");
 
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
-	HomeSysPlatform.setHomebridge(homebridge);
     PlaybulbAccessory.setHomebridge(homebridge);
-
     homebridge.registerAccessory("playbulb", "Playbulb", PlaybulbAccessory);
-}
+};
